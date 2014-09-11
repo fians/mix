524032d1b8bd417d1d19173a9a3ebd607ccf334a
Alfiana Sibuea <alfian.sibuea@gmail.com>
2014-09-12 02:19:40 +0700

    Make the spinner working

--- script.js
+++ script.js
@@ -2,5 +2,38 @@
 ;(function(w, $) {
 	'use strict';
 
-	
+	var slice = 0;
+	var loaderRunning = true;
+
+	function spinLoader() {
+
+		var el = $('#loading .spinner .slice'+slice);
+
+		if (!el.length) {
+			return false;
+		}
+
+		el.addClass('colored');
+
+		slice++;
+		if (slice === 7) {
+			slice = 1;
+		}
+
+		setTimeout(function() {
+			spinLoader(slice);
+		}, 250);
+
+		if (loaderRunning) {
+			setTimeout(function() {
+				el.removeClass('colored');
+			}, 500);
+		}
+	}
+
+	spinLoader();
+
+	setTimeout(function() {
+		loaderRunning = false;
+	}, 10000);
 })(window, jQuery);
